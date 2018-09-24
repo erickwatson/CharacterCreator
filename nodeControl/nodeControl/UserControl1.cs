ca4c7c5734374e2cdfadd40f0a8ad6b041f51ede
Erick Watson <fredmackay@hotmail.com>
2018-09-24 16:32:05 +0930

    Added Nodecontrol

---
+++ nodeControl/nodeControl/UserControl1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nodeControl
+{
+
+    
+    public partial class UserControl1: UserControl
+    {
+        private Point currentCoordinates = new Point(0, 0);
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            e.Graphics.DrawArc(Pens.Black,
+                new Rectangle(currentCoordinates, new Size(5, 5)), 0, 360);
+
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            if (e.GetType() == typeof(MouseEventArgs))
+            {
+                MouseEventArgs me = e as MouseEventArgs;
+                currentCoordinates = me.Location;
+                Refresh();
+            }
+
+            base.OnClick(e);
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
